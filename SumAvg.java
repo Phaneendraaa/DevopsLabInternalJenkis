9b950f0f8ad4d720708acc2228627b09b1ea26b2
Phaneendraaa <nagaphaneendrapuranam@gmail.com>
2025-06-17 15:32:03 +0530

    Added java file

---
+++ SumAvg.java
@@ -0,0 +1,13 @@
+public class SumAvg{
+	public static void main(String[] args){
+		System.out.println("The sum of 1st 10 nums is ");
+		int sum = 0;
+		for(int i=1;i<11;i++){
+			sum+=i;
+		}
+		System.out.println(sum);
+		 System.out.println("Avg of 1st 10 nums is");
+		 System.out.println(sum/10);
+	}
+}
+
